252aff673029d45df58bce6a46f1b540aa06f71d
praveendeepak <koripallipraveendeepak@gmail.com>
2025-12-14 19:57:32 +0530

    Handles linear allocation

--- heap.c
+++ heap.c
@@ -3,7 +3,7 @@
 #include<stdint.h>
 #include<assert.h>
 
-static inline uintptr_t allign_forward_uintptr(uintptr_t ptr, size_t alignmenet){
+static inline uintptr_t align_forward_uintptr(uintptr_t ptr, size_t alignment){
 	if(alignment <= 1) return ptr;
 	assert((alignment & (alignment-1)) == 0 && "alignment must be power of two");
 	uintptr_t mask = (uintptr_t)alignment - 1;
@@ -20,13 +20,13 @@ void la_reset(LinearAllocator *la){
     	la->offset = 0;//setting the bump pointer to 0
 }
 
-void *la_alloc(LinearAllocator *la, size_t size, size_t alignment){//returns a pointer for the process 
+void* la_alloc(LinearAllocator *la, size_t size, size_t alignment){//returns a pointer for the process 
 	if (size == 0) return NULL;
 	if (alignment == 0) alignment = 1;//error handeling
 
 	uintptr_t base = (uintptr_t)la->buffer; 
 	uintptr_t cur_ptr = base + la->offset;
-	uintptr_t aligned = align_forward_unintptr(cur_ptr,alignment);//returns the address of the aligned pointer
+	uintptr_t aligned = align_forward_uintptr(cur_ptr,alignment);//returns the address of the aligned pointer
 	size_t adjustment = (size_t)(aligned - cur_ptr);//padding 
 
 	if(la->offset + adjustment + size > la->capacity){//current+padding+required > capacity
@@ -38,5 +38,5 @@ void *la_alloc(LinearAllocator *la, size_t size, size_t alignment){//returns a p
 
 }
 
-size_t la_capacity(LinearAllocator* la) { return la->capacity; }
-size_t la_offset(LinearAllocator* la) { return la->offset; }
+size_t la_capacity(const LinearAllocator* la) { return la->capacity; }
+size_t la_offset(const LinearAllocator* la) { return la->offset; }
